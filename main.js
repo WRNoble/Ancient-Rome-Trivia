c7c09c15eec815d225b4e49f3142b2d0fa51b38c
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-12 15:27:59 -0400

    checked to ensure the board works

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let cards = ["Which river is Julius Caesar famous for crossing?", "Which Roman general defeated Hannibal Barca at the Battle of Zama?", "This Roman Emperor wrote Meditations?"];
 let answers = ["Rubicon", "Scipio", "Marcus Aurelius"];
 let	rightCards = [];
-console.log(rightCards)
+let board = document.querySelector('.board')
+console.log(board.outerHTML)
 
 //loop through the cards array
 
 for(let i = 0; i <= cards.length; i++) {
 	let question = i
+	board.textContent(question)
 }
 
 //present the first question and prompt the user for an answer
