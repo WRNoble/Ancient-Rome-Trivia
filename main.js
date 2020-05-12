9f7b8f2169dab2606165eb9cb64c4ab96db87c8d
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-12 15:20:48 -0400

    basic HTML and variables

---
+++ main.js
@@ -0,0 +1,16 @@
+let cards = ["Which river is Julius Caesar famous for crossing?", "Which Roman general defeated Hannibal Barca at the Battle of Zama?", "This Roman Emperor wrote Meditations?"];
+let answers = ["Rubicon", "Scipio", "Marcus Aurelius"];
+let	rightCards = [];
+console.log(rightCards)
+
+//loop through the cards array
+
+for(let i = 0; i <= cards.length; i++) {
+	let question = i
+}
+
+//present the first question and prompt the user for an answer
+
+//compares the input with the answer.
+
+//score trackers prints the length of rightCards array.
