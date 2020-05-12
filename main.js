90b1585672fd129c3cfb651853b384e80035a026
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-12 16:02:31 -0400

    created user answer array

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 let cards = ["Which river is Julius Caesar famous for crossing?", "Which Roman general defeated Hannibal Barca at the Battle of Zama?", "This Roman Emperor wrote Meditations?"];
 let answers = ["Rubicon", "Scipio", "Marcus Aurelius"];
+let userAnswer = [];
 let	rightCards = [];
 let wrongCards = [];
 let scoreTracker = [];
 let board = document.querySelector('.board')
-
+console.log(userAnswer)
 
 
 
 //loop through the cards array
 
 for(let i = 0; i < cards.length; i++) {
-	let question = cards[i]
-
-	board.innerHTML = question  //present the first question and prompt the user for an answer
-
-
+	board.innerHTML = cards[i]  //present the first question and prompt the user for an answer
+	//if()
 }
 								//compares the input with the answer.
 
