ef7600ef146cee10a2d3df03d16e36d364944124
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-12 18:41:09 -0400

    added prevent default

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 let cards = [
-"Which river is Julius Caesar famous for crossing?",
-"The first christian Emperor?" 
-"Which Roman general defeated Hannibal Barca at the Battle of Zama?", 
-"This Roman Emperor wrote Meditations?", 
-"The name of the general who led elephants over the Alps to attack Rome?",
-"The first Emperor of the Roman Empire?",
-"This military unit was the backbone of the Roman military"
+{question: "Which river is Julius Caesar famous for crossing?"},
+{question: "The first Christian Emperor?"}, 
+{question: "Which Roman general defeated Hannibal Barca at the Battle of Zama?"}, 
+{question: "This Roman Emperor wrote Meditations?"}, 
+{question: "The name of the general who led elephants over the Alps to attack Rome?"},
+{question: "The first Emperor of the Roman Empire?"},
+{question: "This military unit was the backbone of the Roman military"}
 ];
+
 let answers = [
-"RUBICON",
-"CONSTANTINE", 
-"SCIPIO", 
-"MARCUS AURELIUS", 
-"HANNIBAL",
-"AUGUSTUS",
-"LEGION"
+{answer: "RUBICON"},
+{answer: "CONSTANTINE"}, 
+{answer: "SCIPIO"}, 
+{answer: "MARCUS AURELIUS"}, 
+{answer: "HANNIBAL"},
+{answer: "AUGUSTUS"},
+{answer: "LEGION"}
 ];
-let wrongCards = [];
+
+let playerAnswers = [];
 let scoreTracker = [];
 let board = document.querySelector('.board');
-let userAnswer = document.querySelector('.class');
+let userAnswer = document.querySelector('.answer').value.toUpperCase;
+let button = document.querySelector(".submit");
+button.addEventListener('click', function(value) {
+	.preventDefault()
+	playerAnswers.push(userAnswer)
+})
+console.log(userAnswer)
+
 
 
 
@@ -27,7 +36,14 @@ let userAnswer = document.querySelector('.class');
 //loop through the cards array
 
 for(let i = 0; i < cards.length; i++) {
-	board.innerHTML = cards[i]  //present the first question and prompt the user for an answer
+	board.innerHTML = cards[i].question  //present the first question and prompt the user for an answer
+	break
+	if (userAnswer === answers) {
+		cards.pop(i).question
+		scoreTracker.push(i)
+	} else {
+
+	}
 	// for(let j = 0; j < answers.length; i++){
 	// 	if (userAnswer === answers[j]) {
 	// 		//push question into scoreTracker array
