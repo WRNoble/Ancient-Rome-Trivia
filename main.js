85e10636c8df3fa6f63ccfdd1331bf33c78775bd
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-12 15:38:44 -0400

    added tracker variable

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 let cards = ["Which river is Julius Caesar famous for crossing?", "Which Roman general defeated Hannibal Barca at the Battle of Zama?", "This Roman Emperor wrote Meditations?"];
 let answers = ["Rubicon", "Scipio", "Marcus Aurelius"];
 let	rightCards = [];
-let board = document.querySelector('.board')
+let wrongCards = [];
+let div = document.querySelector('.board')
+let scoreTracker = document.querySelector('.tracker')
+console.log(scoreTracker)
 
 
 //loop through the cards array
 
 for(let i = 0; i <= cards.length; i++) {
 	let question = cards[i]
-	console.log(question)
-	board.textContent(question)
+	div.textContent(question)  //present the first question and prompt the user for an answer
+	console.log(board)
 }
+								//compares the input with the answer.
 
-//present the first question and prompt the user for an answer
 
-//compares the input with the answer.
 
 //score trackers prints the length of rightCards array.
+
