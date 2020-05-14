10585130df6d4348eb35486e4b3e223e6ffd5e5d
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-14 14:37:05 -0400

    if this is a bad idea then revert to commit before this one

--- main.js
+++ main.js
@@ -1,55 +1,67 @@
 let cards = [
 {
 	question: "Which river is Julius Caesar famous for crossing?",
- 	answer: "RUBICON"
+ 	answer: "RUBICON",
+ 	hint: "This river begins with the letter R"
 },
 {
 	question: "Who was the first Christian Emperor?",
  	answer: "CONSTANTINE",
  	answer: "PHILLIP",
- 	answer: "PHILIP"
+ 	answer: "PHILIP",
+ 	hint: "This man has a city named after himself."
 }, 
 {
 	question: "Which Roman general defeated Hannibal at the Battle of Zama?",
  	answer: "SCIPIO",
- 	answer: "SCIPIO AFRICANUS"
+ 	answer: "SCIPIO AFRICANUS",
+ 	hint: "This man survived the battle of Cannae."
 }, 
 {
 	question: "This Roman Emperor wrote Meditations?",
- 	answer: "MARCUS AURELIUS"
+ 	answer: "MARCUS AURELIUS",
+ 	hint: "This emperor was feature in the film 'Gladiator.'"
 }, 
 {
 	question: "The man who led elephants over the Alps to attack Rome?",
- 	answer: "HANNIBAL"
+ 	answer: "HANNIBAL",
+ 	hint: "This man shares his name with a fictional cannibal."
 },
 {
 	question: "The first Emperor of the Roman Empire?",
  	answer: "AUGUSTUS",
- 	answer: "OCTAVIAN"
+ 	answer: "OCTAVIAN",
+ 	hint: "He was the adopted heir of Caesar."
 },
 {
 	question: "This military unit was the backbone of the Roman military",
- 	answer: "LEGION"
+ 	answer: "LEGION",
+ 	hint: "The word became synonymous with the word 'many'."
 },
 {
 	question: "Which Emperor took the Empire to its territorial heights?",
-	answer: "TRAJAN"
+	answer: "TRAJAN",
+	hint: "He was one of the five good emperors of Rome."
 },
 {
 	question: "Which Emperor built a coast-to-coast wall in Britain?",
-	answer: "HADRIAN"
+	answer: "HADRIAN",
+	hint: "He reigned after Trajan."
 },
 {
 	question: "Before the Emperors, what kind of government did Rome have?",
-	answer: "REPUBLIC"
+	answer: "REPUBLIC",
+	hint: "the term in Latin was 'res publica.'"
 }
 ];
 
+let wrong = [];
 let scoreTracker = [];
 let points = document.querySelector(".tracker");
 let board = document.querySelector('.question-text');
 let response = document.querySelector('.response');
 let button = document.querySelector(".submit");
+let hint = document.querySelector(".hint");
 
 button.addEventListener('click', function(e) {
 	e.preventDefault()
@@ -67,17 +79,31 @@ button.addEventListener('click', function(e) {
 			} else {
 				displayQuestion()
 			}
-	} else {
+	} else if (answer2 !== cards[0].answer) {
+		hint.textContent = cards[0].hint;
 		response.textContent = "Sorry, you answered that question incorrectly."
-		let object2 = cards.shift()
-		cards.push(object2)
-		displayQuestion()
-	}
+		if (answer2 === cards[0].answer) {
+			let object = cards.shift()
+			scoreTracker.unshift(object)
+			let pointTracker = scoreTracker.length
+			points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
+			response.textContent = "Congratulations, you have answered that question correctly!"
+		} else {
+			response.textContent = "Sorry, you answered that question incorrectly."
+			let object2 = cards.shift()
+			cards.push(object2)
+			displayQuestion()
+		}
+	// } else {
+	// 	response.textContent = "Sorry, you answered that question incorrectly."
+	// 	let object2 = cards.shift()
+	// 	cards.push(object2)
+	// 	displayQuestion()
+	// }
+	}//)
 })
-
 function displayQuestion() {
 		board.innerHTML = cards[0].question
 }
 
 displayQuestion()
-//timer.addEventListener('click', )
