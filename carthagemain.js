5e877f3537f789fe71e0f3c9f3514531b29e7fc8
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-14 16:13:09 -0400

    added wrong array on second version

--- carthagemain.js
+++ carthagemain.js
@@ -1,8 +1,58 @@
 let cards = [
-{}
+{
+	question: "Which river is Julius Caesar famous for crossing?",
+ 	answer: "RUBICON",
+ 	hint: "This river begins with the letter R"
+},
+{
+	question: "Who was the first Christian Emperor?",
+ 	answer: "CONSTANTINE",
+ 	hint: "This man has a city named after himself."
+}, 
+{
+	question: "Which Roman general defeated Hannibal at the Battle of Zama?",
+ 	answer: "SCIPIO",
+ 	hint: "This man survived the battle of Cannae."
+}, 
+{
+	question: "This Roman Emperor wrote Meditations?",
+ 	answer: "MARCUS AURELIUS",
+ 	hint: "This emperor was feature in the film 'Gladiator.'"
+}, 
+{
+	question: "The man who led elephants over the Alps to attack Rome?",
+ 	answer: "HANNIBAL",
+ 	hint: "This man shares his name with a fictional cannibal."
+},
+{
+	question: "The first Emperor of the Roman Empire?",
+ 	answer: "AUGUSTUS",
+ 	hint: "He was the adopted heir of Caesar."
+},
+{
+	question: "This military unit was the backbone of the Roman military",
+ 	answer: "LEGION",
+ 	hint: "The word became synonymous with the word 'many'."
+},
+{
+	question: "Which Emperor took the Empire to its territorial heights?",
+	answer: "TRAJAN",
+	hint: "He was one of the five good emperors of Rome."
+},
+{
+	question: "Which Emperor built a coast-to-coast wall in Britain?",
+	answer: "HADRIAN",
+	hint: "He reigned after Trajan."
+},
+{
+	question: "Before the Emperors, what kind of government did Rome have?",
+	answer: "REPUBLIC",
+	hint: "the term in Latin was 'res publica.'"
+}
 ];
 
 let scoreTracker = [];
+let wrong = [];
 let points = document.querySelector(".tracker");
 let board = document.querySelector('.question-text');
 let response = document.querySelector('.response');
@@ -27,7 +77,8 @@ button.addEventListener('click', function(e) {
 	} else {
 		response.textContent = "Sorry, you answered that question incorrectly."
 		let object2 = cards.shift()
-		cards.push(object2)
+		wrong.push(object2)
+		console.log(wrong)
 		displayQuestion()
 	}
 })
--- main.js
+++ main.js
@@ -55,13 +55,14 @@ let cards = [
 }
 ];
 
-let wrong = [];
 let scoreTracker = [];
 let points = document.querySelector(".tracker");
 let board = document.querySelector('.question-text');
 let response = document.querySelector('.response');
 let button = document.querySelector(".submit");
 let hint = document.querySelector(".hint");
+
+
 
 button.addEventListener('click', function(e) {
 	e.preventDefault()
@@ -79,31 +80,55 @@ button.addEventListener('click', function(e) {
 			} else {
 				displayQuestion()
 			}
-	} else if (answer2 !== cards[0].answer) {
-		hint.textContent = cards[0].hint;
+	} else {
 		response.textContent = "Sorry, you answered that question incorrectly."
-		if (answer2 === cards[0].answer) {
-			let object = cards.shift()
-			scoreTracker.unshift(object)
-			let pointTracker = scoreTracker.length
-			points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
-			response.textContent = "Congratulations, you have answered that question correctly!"
-		} else {
-			response.textContent = "Sorry, you answered that question incorrectly."
-			let object2 = cards.shift()
-			cards.push(object2)
-			displayQuestion()
-		}
-	// } else {
-	// 	response.textContent = "Sorry, you answered that question incorrectly."
-	// 	let object2 = cards.shift()
-	// 	cards.push(object2)
-	// 	displayQuestion()
-	// }
-	}//)
+		let object2 = cards.shift()
+		cards.push(object2)
+		displayQuestion()
+	}
 })
+
+
 function displayQuestion() {
-		board.innerHTML = cards[0].question
+	board.innerHTML = cards[0].question
 }
 
 displayQuestion()
+
+// function rightAnswer() {
+// 	let userAnswer = document.querySelector('.answer').value
+// 	let answer2 = userAnswer.toUpperCase();
+// 	if (answer2 === cards[0].answer) {
+// 		let object = cards.shift()
+// 		scoreTracker.unshift(object)
+// 		let pointTracker = scoreTracker.length
+// 		points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
+// 		response.textContent = "Congratulations, you have answered that question correctly!"
+// 		for(let i = 0; i <= cards.length; i++)
+// 			if(cards.length === 0) {
+// 				board.textContent = "Venisti, vidi, vicit"
+// 			} else {
+// 				displayQuestion()
+// 			}
+// 	}
+// }
+// function wrongAnswer() {
+// 	let userAnswer = document.querySelector('.answer').value
+// 	let answer2 = userAnswer.toUpperCase();
+// 	if (answer2 !== cards[0].answer) {
+// 		hint.textContent = cards[0].hint;
+// 		response.textContent = "Sorry, you answered that question incorrectly."
+// 	}
+// }
+
+// function wrongAnswer2() {
+// 	let userAnswer = document.querySelector('.answer').value
+// 	let answer2 = userAnswer.toUpperCase();
+// 	if (answer2 !== cards[0].answer) {
+// 		let wrongCard = cards.shift()
+// 		wrong.push(wrongCard)
+// 		response.textContent = "Sorry, you answered that question incorrectly."
+// 	}
+// }
+
+
