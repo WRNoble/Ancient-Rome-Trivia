0e2e70bb68244bc4e412e9171abd4436ab873d2f
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-15 10:11:14 -0400

    disabled submit button

--- main.js
+++ main.js
@@ -1,94 +1,91 @@
+//Array establishing the questions
 let cards = [
 {
 	question: "Which river is Julius Caesar famous for crossing?",
- 	answer: "RUBICON",
- 	hint: "This river begins with the letter R"
+ 	answer: "RUBICON"
 },
 {
 	question: "Who was the first Christian Emperor?",
- 	answer: "CONSTANTINE",
- 	otherAnswer: "PHILIP",
- 	hint: "This man has a city named after himself."
-}, 
+ 	answer: "CONSTANTINE"
+},
 {
 	question: "Which Roman general defeated Hannibal at the Battle of Zama?",
- 	answer: "SCIPIO",
- 	otherAnswer: "SCIPTIO AFRICANUS",
- 	hint: "This man survived the battle of Cannae."
+ 	answer: "SCIPIO"
 }, 
 {
 	question: "This Roman Emperor wrote Meditations?",
- 	answer: "MARCUS AURELIUS",
- 	hint: "This emperor was feature in the film 'Gladiator.'"
+ 	answer: "MARCUS AURELIUS"
 }, 
 {
 	question: "The man who led elephants over the Alps to attack Rome?",
- 	answer: "HANNIBAL",
- 	hint: "This man shares his name with a fictional cannibal."
+ 	answer: "HANNIBAL"
 },
 {
 	question: "The first Emperor of the Roman Empire?",
- 	answer: "AUGUSTUS",
- 	otherAnswer: "OCTAVIAN",
- 	hint: "He was the adopted heir of Caesar."
+ 	answer: "AUGUSTUS"
 },
 {
 	question: "This military unit was the backbone of the Roman military",
- 	answer: "LEGION",
- 	hint: "The word became synonymous with the word 'many'."
+ 	answer: "LEGION"
 },
 {
 	question: "Which Emperor took the Empire to its territorial heights?",
-	answer: "TRAJAN",
-	hint: "He was one of the five good emperors of Rome."
+	answer: "TRAJAN"
 },
 {
 	question: "Which Emperor built a coast-to-coast wall in Britain?",
-	answer: "HADRIAN",
-	hint: "He reigned after Trajan."
+	answer: "HADRIAN"
 },
 {
 	question: "Before the Emperors, what kind of government did Rome have?",
-	answer: "REPUBLIC",
-	hint: "the term in Latin was 'res publica.'"
+	answer: "REPUBLIC"
 }
 ];
-
+//establishing important variables before the logic.
 let scoreTracker = [];
 let wrong = [];
 let points = document.querySelector('.tracker');
 let board = document.querySelector('.question-text');
 let response = document.querySelector('.response');
 let button = document.querySelector('.submit');
-
+//game logic comes last to ensure everything has been declared.
 button.addEventListener('click', function(e) {
-	e.preventDefault()
-	let userAnswer = document.querySelector('.answer').value
-	let answer2 = userAnswer.toUpperCase();
-	if (answer2 === cards[0].answer || cards[0].otherAnswer) {
-		let object = cards.shift()
-		scoreTracker.unshift(object)
-		let pointTracker = scoreTracker.length
-		points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
-		response.textContent = "Congratulations, you have answered that question correctly!"
-		win()
-	} else {
-		response.textContent = "Sorry, you answered that question incorrectly."
-		let object2 = cards.shift()
-		wrong.push(object2)
-		win()
-	}
+	e.preventDefault();
+	let userAnswer = document.querySelector('.answer').value;
+	let input = userAnswer.toUpperCase();
+	moveCard(input);
 })
-
+//needs functions are found below the event listener
 function displayQuestion() {
-	board.innerHTML = cards[0].question	
+	board.innerHTML = cards[0].question	;
 }
 
 displayQuestion()
 
+function moveCard(input) {
+	if (input === cards[0].answer) {
+		let cardInPlay = cards.shift();
+		scoreTracker.unshift(cardInPlay);
+		let pointTracker = scoreTracker.length;
+		points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`;
+		response.textContent = "Congratulations, you have answered that question correctly!";
+		win();
+	} else {
+		response.textContent = "Sorry, you answered that question incorrectly.";
+		let wrongAnswer = cards.shift();
+		wrong.push(wrongAnswer);
+		win();
+	}
+}
+
+function setTimer() {
+
+}
+
 function win() {
 	if(cards.length === 0 && scoreTracker.length >= 6) {
 		board.textContent = "Venisti, vidi, vicit!"
+		document.querySelector('.submit').disabled = true;
 	} else if (cards.length === 0 && scoreTracker.length <= 5){
 		board.textContent = "You did not survive the barbarian invasion."
 	} else {
