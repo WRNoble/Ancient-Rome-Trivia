82c753157308b35d5c2ec2715b2f7651752256c1
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-14 23:09:59 -0400

    done for the day it works well

--- main.js
+++ main.js
@@ -7,14 +7,13 @@ let cards = [
 {
 	question: "Who was the first Christian Emperor?",
  	answer: "CONSTANTINE",
- 	answer: "PHILLIP",
- 	answer: "PHILIP",
+ 	otherAnswer: "PHILIP",
  	hint: "This man has a city named after himself."
 }, 
 {
 	question: "Which Roman general defeated Hannibal at the Battle of Zama?",
  	answer: "SCIPIO",
- 	answer: "SCIPIO AFRICANUS",
+ 	otherAnswer: "SCIPTIO AFRICANUS",
  	hint: "This man survived the battle of Cannae."
 }, 
 {
@@ -30,7 +29,7 @@ let cards = [
 {
 	question: "The first Emperor of the Roman Empire?",
  	answer: "AUGUSTUS",
- 	answer: "OCTAVIAN",
+ 	otherAnswer: "OCTAVIAN",
  	hint: "He was the adopted heir of Caesar."
 },
 {
@@ -56,79 +55,43 @@ let cards = [
 ];
 
 let scoreTracker = [];
-let points = document.querySelector(".tracker");
+let wrong = [];
+let points = document.querySelector('.tracker');
 let board = document.querySelector('.question-text');
 let response = document.querySelector('.response');
-let button = document.querySelector(".submit");
-let hint = document.querySelector(".hint");
-
-
+let button = document.querySelector('.submit');
 
 button.addEventListener('click', function(e) {
 	e.preventDefault()
 	let userAnswer = document.querySelector('.answer').value
 	let answer2 = userAnswer.toUpperCase();
-	if (answer2 === cards[0].answer) {
+	if (answer2 === cards[0].answer || cards[0].otherAnswer) {
 		let object = cards.shift()
 		scoreTracker.unshift(object)
 		let pointTracker = scoreTracker.length
 		points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
 		response.textContent = "Congratulations, you have answered that question correctly!"
-		for(let i = 0; i <= cards.length; i++)
-			if(cards.length === 0) {
-				board.textContent = "Venisti, vidi, vicit"
-			} else {
-				displayQuestion()
-			}
+		win()
 	} else {
 		response.textContent = "Sorry, you answered that question incorrectly."
 		let object2 = cards.shift()
-		cards.push(object2)
-		displayQuestion()
+		wrong.push(object2)
+		win()
 	}
 })
 
-
 function displayQuestion() {
-	board.innerHTML = cards[0].question
+	board.innerHTML = cards[0].question	
 }
 
 displayQuestion()
 
-// function rightAnswer() {
-// 	let userAnswer = document.querySelector('.answer').value
-// 	let answer2 = userAnswer.toUpperCase();
-// 	if (answer2 === cards[0].answer) {
-// 		let object = cards.shift()
-// 		scoreTracker.unshift(object)
-// 		let pointTracker = scoreTracker.length
-// 		points.innerHTML = `You have answered ${pointTracker} out of 10 correctly thus far!`
-// 		response.textContent = "Congratulations, you have answered that question correctly!"
-// 		for(let i = 0; i <= cards.length; i++)
-// 			if(cards.length === 0) {
-// 				board.textContent = "Venisti, vidi, vicit"
-// 			} else {
-// 				displayQuestion()
-// 			}
-// 	}
-// }
-// function wrongAnswer() {
-// 	let userAnswer = document.querySelector('.answer').value
-// 	let answer2 = userAnswer.toUpperCase();
-// 	if (answer2 !== cards[0].answer) {
-// 		hint.textContent = cards[0].hint;
-// 		response.textContent = "Sorry, you answered that question incorrectly."
-// 	}
-// }
-
-// function wrongAnswer2() {
-// 	let userAnswer = document.querySelector('.answer').value
-// 	let answer2 = userAnswer.toUpperCase();
-// 	if (answer2 !== cards[0].answer) {
-// 		let wrongCard = cards.shift()
-// 		wrong.push(wrongCard)
-// 		response.textContent = "Sorry, you answered that question incorrectly."
-// 	}
-// }
-
-
+function win() {
+	if(cards.length === 0 && scoreTracker.length >= 6) {
+		board.textContent = "Venisti, vidi, vicit!"
+	} else if (cards.length === 0 && scoreTracker.length <= 5){
+		board.textContent = "You did not survive the barbarian invasion."
+	} else {
+		displayQuestion()
+	}
+}
